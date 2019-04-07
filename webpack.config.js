a41f039fa1231eb2ec3bab160ce861e210a30dc8
Ilya Kushlianski <kushliansky@gmail.com>
2019-04-07 13:58:05 +0300

    WNB-67: Set up basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackMd5Hash = require("webpack-md5-hash");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const distFolder = "dist";
+
+module.exports = {
+  entry: { main: "./src/index.js" },
+  output: {
+    path: path.resolve(__dirname, distFolder),
+    filename: "[name].[hash].js",
+    chunkFilename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        vendors: false,
+        vendor: {
+          // sync + async chunks
+          chunks: "all",
+          // import file path containing node_modules
+          test: /node_modules/,
+          name: "vendor"
+        }
+      }
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      inject: true,
+      hash: true,
+      template: "./public/index.html",
+      filename: "index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css"
+    }),
+    new WebpackMd5Hash(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
+    open: true
+  }
+};
