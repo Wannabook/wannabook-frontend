41113a1169b4fcac582d83f0236e79eb701daf67
Ilya Kushlianski <kushliansky@gmail.com>
2019-04-16 22:41:41 +0300

    WNB-99: Add support for .env files in project

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,92 @@
+const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const dotenv = require('dotenv');
 
 const devMode = process.env.NODE_ENV !== 'production';
 const distFolder = 'dist';
 
-module.exports = {
-  entry: { main: './src/index.js' },
-  output: {
-    path: path.resolve(__dirname, distFolder),
-    filename: '[name].[hash].js',
-    chunkFilename: '[name].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js|jsx$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+module.exports = env => {
+  // Get the root path (assuming your webpack config is in the root of your project!)
+  const currentPath = path.join(__dirname);
+
+  // Create the fallback path (the production .env)
+  const basePath = currentPath + '/.env';
+
+  // We're concatenating the environment name to our filename to specify the correct env file!
+  const envPath = basePath + '.' + env.ENVIRONMENT;
+
+  // Check if the file exists, otherwise fall back to the production .env
+  const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+
+  // Set the path parameter in the dotenv config
+  const fileEnv = dotenv.config({ path: finalPath }).parsed;
+
+  // reduce it to a nice object, the same as before (but with the variables from the file)
+  const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+
+    return prev;
+  }, {});
+
+  return {
+    entry: { main: './src/index.js' },
+    output: {
+      path: path.resolve(__dirname, distFolder),
+      filename: '[name].[hash].js',
+      chunkFilename: '[name].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js|jsx$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-          },
-          {
-            loader: 'sass-loader',
-          },
-        ],
-      },
+        {
+          test: /\.scss$/,
+          use: [
+            devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx'],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        inject: 'body',
+        hash: true,
+        template: './public/index.html',
+        filename: 'index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'style.css',
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+      new WebpackMd5Hash(),
+      new webpack.DefinePlugin(envKeys),
     ],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx'],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      inject: 'body',
-      hash: true,
-      template: './public/index.html',
-      filename: 'index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new WebpackMd5Hash(),
-  ],
-  devServer: {
-    contentBase: './dist',
-    hot: true,
-    open: false,
-    historyApiFallback: true,
-  },
-  devtool: 'eval-source-map',
+    devServer: {
+      contentBase: './dist',
+      hot: true,
+      open: false,
+      historyApiFallback: true,
+    },
+    devtool: 'eval-source-map',
+  };
 };
