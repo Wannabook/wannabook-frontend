40881d7e20b20633061e48c10eeb9021040df0f0
dmitri_lipski <dzmitri.lipski@gmail.com>
2019-04-21 19:38:48 +0300

    WNB-87: Do some refactor for Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 
 import Navigation from '../Navigation';
-
-// const StyledHeader = styled.header`
-//   height: 60px;
-//   border: 2px solid blue;
-// `;
 
 const Header = props => {
   return (
