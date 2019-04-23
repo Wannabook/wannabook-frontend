50223456ca215c7378c4efbdfa2ce4b77dc2ad05
Ilya Kushlianski <kushliansky@gmail.com>
2019-04-24 01:45:02 +0300

    WNB-86: Refactor routes, place them into separate file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
-import loadable from 'react-loadable';
+import { BrowserRouter } from 'react-router-dom';
 import { Normalize } from 'styled-normalize';
 
+import AppRoutes from './routes/index';
 import Header from './components/Header';
-import Loading from './components/Loading';
 
 import store from './store';
 
@@ -14,55 +13,16 @@ import 'semantic-ui-css/semantic.min.css';
 
 // TODO: Do we need withRouter anywhere here?
 class App extends React.Component {
-  componentDidMount() {
-    console.log('process.env.API_URL', process.env.API_URL);
-  }
-
   render() {
     return (
       <>
         <Normalize />
-        {/*We should abstract away this header and its contents into a separate component*/}
-        {/*Same with the list of routes that I'm now doing. This list should be somewhere in a separate place */}
         <Header />
-        <Switch>
-          <Route
-            path="/"
-            exact
-            component={createAsyncComponent('./routes/Home')}
-          />
-          <Route
-            path="/companies"
-            component={createAsyncComponent('./routes/Companies')}
-          />
-          <Route
-            path="/company/:id"
-            component={createAsyncComponent('./routes/Company')}
-          />
-          <Route
-            path="/user/:id"
-            component={createAsyncComponent('./routes/User')}
-          />
-          <Route
-            path="/login"
-            component={createAsyncComponent('./routes/Login')}
-          />
-          <Route
-            path="/logout"
-            component={createAsyncComponent('./routes/Logout')}
-          />
-          <Route component={createAsyncComponent('./routes/NotFound')} />
-        </Switch>
+        <AppRoutes />
       </>
     );
   }
 }
-
-const createAsyncComponent = url =>
-  loadable({
-    loader: () => import(/* webpackChunkName: "[request][index]" */ `${url}`),
-    loading: Loading,
-  });
 
 render(
   <Provider store={store}>
---
+++ src/routes/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import loadable from 'react-loadable';
+
+import Loading from '../components/Loading';
+
+export default function AppRoutes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={createAsyncComponent('./Home')} />
+      <Route
+        path="/companies"
+        component={createAsyncComponent('./Companies')}
+      />
+      <Route
+        path="/company/:id"
+        component={createAsyncComponent('./Company')}
+      />
+      <Route path="/services" component={createAsyncComponent('./Services')} />
+      <Route
+        path="/categories"
+        component={createAsyncComponent('./Categories')}
+      />
+      <Route
+        path="/service/:id"
+        component={createAsyncComponent('./Service')}
+      />
+      <Route path="/user/:id" component={createAsyncComponent('./User')} />
+      <Route path="/login" component={createAsyncComponent('./Login')} />
+      <Route path="/logout" component={createAsyncComponent('./Logout')} />
+      <Route component={createAsyncComponent('./NotFound')} />
+    </Switch>
+  );
+}
+
+const createAsyncComponent = url =>
+  loadable({
+    loader: () => import(/* webpackChunkName: "[request][index]" */ `${url}`),
+    loading: Loading,
+  });
