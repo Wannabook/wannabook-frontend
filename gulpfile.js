04f9b93477115c2f5173e72d6eebe2711af20407
Ilya Kushlianski <ikushlianski@klika-tech.com>
2019-05-06 15:44:32 +0300

    WNB-112: Replace gulp with webpack browserSync plugin

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync');
-
-gulp.task('mobile', function() {
-  browserSync({
-    proxy: 'localhost:8080',
-    open: false,
-  });
-});
--- webpack.config.js
+++ webpack.config.js
@@ -6,72 +6,76 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
-const devMode = process.env.NODE_ENV !== 'production';
-const isTestingOnMobile = process.env.MOBILE_TESTING == true;
-const distFolder = 'dist';
+module.exports = env => {
+  const devMode = process.env.NODE_ENV !== 'production';
+  const isTestingOnMobile = env && env.MOBILE_TESTING;
+  console.warn('isTestingOnMobile', isTestingOnMobile);
+  const distFolder = 'dist';
 
-module.exports = {
-  entry: { main: './src/index.js' },
-  output: {
-    path: path.resolve(__dirname, distFolder),
-    filename: '[name].[hash].js',
-    chunkFilename: '[name].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js|jsx$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+  const config = {
+    entry: { main: './src/index.js' },
+    output: {
+      path: path.resolve(__dirname, distFolder),
+      filename: '[name].[hash].js',
+      chunkFilename: '[name].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js|jsx$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-          },
-          {
-            loader: 'sass-loader',
-          },
-        ],
-      },
+        {
+          test: /\.scss$/,
+          use: [
+            devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx'],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        inject: 'body',
+        hash: true,
+        template: './public/index.html',
+        filename: 'index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'style.css',
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+      new WebpackMd5Hash(),
     ],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx'],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      inject: 'body',
-      hash: true,
-      template: './public/index.html',
-      filename: 'index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new WebpackMd5Hash(),
-  ],
-  devServer: {
-    contentBase: './dist',
-    hot: true,
-    open: false,
-    historyApiFallback: true,
-  },
-  devtool: 'eval-source-map',
-};
+    devServer: {
+      contentBase: './dist',
+      hot: true,
+      open: false,
+      historyApiFallback: true,
+    },
+    devtool: 'eval-source-map',
+  };
 
-if (isTestingOnMobile) {
-  mobule.exports.plugins.push(
-    new BrowserSyncPlugin({
-      proxy: 'localhost:8080',
-      // open: false,
-      // server: { baseDir: ['public'] }
-    })
-  );
-}
+  if (isTestingOnMobile) {
+    config.plugins.push(
+      new BrowserSyncPlugin({
+        proxy: 'localhost:8080',
+        open: false,
+      })
+    );
+  }
+
+  return config;
+};
