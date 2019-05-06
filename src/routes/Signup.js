28e6db462350ad4adf207b03f18fd5e93655714e
Ilya Kushlianski <kushliansky@gmail.com>
2019-05-06 12:47:32 +0300

    WNB-86: Add todo-comment on registration page

--- src/routes/Signup.js
+++ src/routes/Signup.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default class Signup extends React.Component {
+  // TODO add redirection to "/" in case a user is already registered
   render() {
     return <div>Registration page</div>;
   }
