8ce8fcbaec749dee07c0912454fa38f98d6a8bad
Dmitri Lipski <dlipski@klika-tech.com>
2019-06-28 18:25:34 +0300

    WNB-111: Add handler to User component

--- src/routes/User/User.js
+++ src/routes/User/User.js
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom';
 import { Grid, Segment, Modal, Button } from 'semantic-ui-react';
 import { getPageUrl } from '../RouteResolver';
 import { ShowChangePasswordModalButton } from './styles';
+import {
+  ChangePasswordButton,
+  ChangePasswordForm,
+} from '../ChangePassword/styles';
 
-const ChangePasswordModal = () => (
-  <Modal trigger={<Button>Изменить пароль</Button>}>
+const ChangePasswordModal = ({ open }) => (
+  <Modal open={open}>
     <Modal.Header>Смена пароля</Modal.Header>
     <Modal.Content>
       <Modal.Description>
@@ -17,6 +21,8 @@ const ChangePasswordModal = () => (
 
 const User = () => {
   const [showChangePasswordModal, setShowChangePasswordModal] = useState(false);
+  const handleShowModal = () =>
+    setShowChangePasswordModal(!showChangePasswordModal);
 
   return (
     <Grid stackable columns="1">
@@ -26,7 +32,14 @@ const User = () => {
             <div>User page</div>
             <Link to={getPageUrl('SIGN-OUT')}>Log out</Link>
             {/*<Link to={getPageUrl('CHANGE-PASSWORD')}>Сменить пароль</Link>*/}
-            <ChangePasswordModal />
+            <ChangePasswordModal open={showChangePasswordModal} />
+            <ChangePasswordButton
+              size="large"
+              primary
+              onClick={handleShowModal}
+            >
+              Изменить пароль
+            </ChangePasswordButton>
           </Segment>
         </Grid.Column>
       </Grid.Row>
