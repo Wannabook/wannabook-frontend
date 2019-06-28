9a0a8f09aa347265cfea10bea45e42deb27aee5f
Dmitri Lipski <dlipski@klika-tech.com>
2019-06-28 09:01:54 +0300

    WNB-111: Add PasswordInput component

---
+++ src/components/FormComponents/PasswordInput/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import Input from '../../../ui-kit/components/Input';
+
+export const StyledInput = styled(Input)`
+  &&& {
+    width: 100%;
+    &&& input {
+      border-radius: 0;
+    }
+  }
+`;
