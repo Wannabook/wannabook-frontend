b3eb0ebe43309bad75f14f2b6582594189c5f254
Dmitri Lipski <dlipski@klika-tech.com>
2019-09-30 09:54:17 +0300

    WNB-178: Style authentication page

--- src/routes/Auth/Auth.js
+++ src/routes/Auth/Auth.js
@@ -3,37 +3,37 @@ import { Link } from 'react-router-dom';
 
 import { getPageUrl } from '../RouteResolver';
 
-import { Description, Illustration, SignInButton } from './styles';
+import {
+  RegistrationContainer,
+  Title,
+  Description,
+  Illustration,
+  SignInButton,
+  SignUpButton,
+} from './styles';
 
-const buttons = [
-  {
-    url: getPageUrl('SIGN-IN'),
-    type: { secondary: true, size: 'large' },
-    btnText: 'Войти',
-  },
-  {
-    url: getPageUrl('SIGN-UP'),
-    type: { secondary: true, size: 'large' },
-    btnText: 'Зарегистрироваться',
-  },
-];
+import authImage from './images/auth_image.svg';
 
 const descriptionText =
-  'Наличие профиля поможет Вам без труда бронировать услуги на нашем сайте';
+  'Наличие профиля поможет без труда бронировать услуги на нашем сайте';
 
 const Registration = () => {
   return (
-    <>
-      <Description>
-        <Illustration src="" alt="Illustration" />
-        <p>{descriptionText}</p>
-      </Description>
-      {buttons.map(item => (
-        <Link to={item.url} key={item.url}>
-          <SignInButton {...item.type}>{item.btnText}</SignInButton>
-        </Link>
-      ))}
-    </>
+    <RegistrationContainer>
+      <Title>Всегда в теме!</Title>
+      <Description>{descriptionText}</Description>
+      <Illustration src={authImage} alt="Illustration" />
+      <Link to={getPageUrl('SIGN-IN')} key={getPageUrl('SIGN-IN')}>
+        <SignInButton primary size="large">
+          Войти
+        </SignInButton>
+      </Link>
+      <Link to={getPageUrl('SIGN-UP')} key={getPageUrl('SIGN-UP')}>
+        <SignUpButton secondary size="large">
+          Зарегистрироваться
+        </SignUpButton>
+      </Link>
+    </RegistrationContainer>
   );
 };
 
--- src/ui-kit/theme/default/variables.js
+++ src/ui-kit/theme/default/variables.js
@@ -1,6 +1,5 @@
 //Colors
 export const brandPrimaryColor = '#3300FF';
-export const secondaryColor = '#34B876';
 export const warningColor = '#D9534F';
 export const white = '#FFFFFF';
-export const mainBackgroundColor = '#e5e5e5';
+export const mainBackgroundColor = '#F5F6FA';
