17f1fc9d67daa031ffb87f5a03bf31b6895b6e30
dmitri_lipski <dzmitri.lipski@gmail.com>
2019-10-20 16:19:03 +0300

    WNB-109: Add ProfileLink component

--- src/components/routes/Profile/MainProfilePage/MainProfilePage.js
+++ src/components/routes/Profile/MainProfilePage/MainProfilePage.js
@@ -1,19 +1,71 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { getPageUrl } from '../../../../routes/RouteResolver';
 import { ChangePassword } from '../ChangePassword/ChangePassword';
+import { ProfileLink } from './ProfileLink/ProfileLink';
 
-import { StyledProfileLink, Illustration } from './styles';
 import notifications from './images/notifications.svg';
+import myinfo from './images/my_info_icon.svg';
+import bonuses from './images/bonuses_icon.svg';
+import bookedEvents from './images/booked_events_icon.svg';
+import favorites from './images/favorites_icon.svg';
+import exit from './images/exit_icon.svg';
 
+import { MainProfilePageContainer } from './styles';
+
+const profileLinksData = [
+  {
+    url: '/',
+    text: 'Мои уведомления',
+    icon: notifications,
+    notificationsAmount: 2,
+  },
+  {
+    url: '/',
+    text: 'Моя информация',
+    icon: myinfo,
+    notificationsAmount: null,
+  },
+  {
+    url: '/',
+    text: 'Бонусы и скидки',
+    icon: bonuses,
+    notificationsAmount: null,
+  },
+  {
+    url: '/',
+    text: 'Забронированные события',
+    icon: bookedEvents,
+    notificationsAmount: null,
+  },
+  {
+    url: '/',
+    text: 'Избранное',
+    icon: favorites,
+    notificationsAmount: null,
+  },
+  {
+    url: '/',
+    text: 'Выйти',
+    icon: exit,
+    notificationsAmount: null,
+  },
+];
 export const MainProfilePage = () => {
   return (
-    <>
-      <div>Main profile page</div>
-      <StyledProfileLink to="/">Мои уведомления</StyledProfileLink>
+    <MainProfilePageContainer>
+      {profileLinksData.map(link => {
+        return (
+          <ProfileLink
+            key={link.text}
+            to={link.url}
+            icon={link.icon}
+            notificationsAmount={link.notificationsAmount}
+          >
+            {link.text}
+          </ProfileLink>
+        );
+      })}
       {/*<StyledProfileLink to={getPageUrl('SIGN-OUT')}>Log out</StyledProfileLink>*/}
-      <Illustration src={notifications} alt="Thank you" />
       <ChangePassword />
-    </>
+    </MainProfilePageContainer>
   );
 };
--- src/components/routes/Profile/MainProfilePage/styles.js
+++ src/components/routes/Profile/MainProfilePage/styles.js
@@ -1,22 +1,4 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 import { mainFontColor } from '../../../../ui-kit/theme';
 
-export const StyledProfileLink = styled(Link)`
-  display: block;
-  height: 60px;
-  padding-left: 60px;
-  font-size: 14px;
-  text-align: left;
-  line-height: 60px;
-  color: ${mainFontColor};
-  background-color: #fefeff;
-  border-radius: 10px;
-`;
-
-export const Illustration = styled.img`
-  display: block;
-  margin-bottom: 20px;
-  margin-left: auto;
-  margin-right: auto;
-`;
+export const MainProfilePageContainer = styled.div``;
