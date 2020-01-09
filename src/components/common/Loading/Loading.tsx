6aed25d7b69f18f0f8833483ce879884c94ea6c2
Ilya Kushlianski <kushliansky@gmail.com>
2020-01-10 01:20:35 +0300

    WNB-176: Style the loader that shows during async module import

--- src/components/common/Loading/Loading.tsx
+++ src/components/common/Loading/Loading.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
 
-export const Loading = () => {
-  // TODO: we could generate a set of 5-10 random phrases near the loading spinner
-  // TODO: add spinner?
-  return <p>Ещё чуть-чуть...</p>;
-};
+interface LoadingProps {
+  fixed?: boolean;
+  placeholder?: string;
+}
+
+export const Loading: React.FC<LoadingProps> = ({ placeholder, ...props }) => (
+  <StyledLoader {...props}>
+    <div>{placeholder || 'Загрузка...'}</div>
+  </StyledLoader>
+);
+
+const StyledLoader = styled.div<LoadingProps>`
+  ${props =>
+    props.fixed &&
+    css`
+      background: white;
+      position: absolute;
+      color: rgba(0, 0, 0, 0.25);
+      font-size: 28px;
+      margin: auto;
+      top: 0;
+      left: 0;
+      right: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    `}
+`;
